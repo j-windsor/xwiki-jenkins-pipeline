bd88c4a1546297e77af432e81e726a28b9524783
Jay Windsor <jaywindsor@google.com>
2022-08-16 16:54:11 -0700

    account for master -> built-in rename

--- vars/xwikiBuild.groovy
+++ vars/xwikiBuild.groovy
@@ -414,7 +414,7 @@ private def createFilePath(String path)
 {
     if (env['NODE_NAME'] == null) {
         error "envvar NODE_NAME is not set, probably not inside an node {} or running an older version of Jenkins!"
-    } else if (env['NODE_NAME'].equals("master")) {
+    } else if (env['NODE_NAME'].equals("master") || env['NODE_NAME'].equals("built-in")) {
         return new FilePath(new File(path))
     } else {
         return new FilePath(Jenkins.getInstance().getComputer(env['NODE_NAME']).getChannel(), path)
